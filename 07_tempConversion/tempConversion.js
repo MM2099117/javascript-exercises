e4099905f75c273af290099151c140b4a3a5ad67
Mark <mark.mcnulty1994@gmail.com>
2023-01-02 14:09:40 +0000

    Exercise 7 complete.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,30 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+
+  var CTemp;
+  var workingNumber;
+  var roundedCTemp;
+
+  CTemp = temperature - 32;
+  workingNumber = CTemp/1.8;
+  roundedCTemp = workingNumber.toFixed(1);
+  roundedCTemp = parseFloat(roundedCTemp);
+  return roundedCTemp;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+
+  var FTemp;
+  var workingNumber;
+  var roundedFTemp;
+
+  FTemp = temperature * 1.8;
+  workingNumber = FTemp + 32;
+  roundedFTemp = workingNumber.toFixed(1);
+  roundedFTemp = parseFloat(roundedFTemp);
+  return roundedFTemp;
+
 };
 
 // Do not edit below this line
