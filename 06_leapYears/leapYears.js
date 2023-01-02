22a63e8140a40097b5d40f2ef57bbe7b67d1edc1
Mark <mark.mcnulty1994@gmail.com>
2023-01-02 11:35:25 +0000

    Exercise 6 complete!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //Task: Create a function that determines whether or not a given year is a leap year. Leap years are determined by the following rules:
+
+    //rules: leap year is divisible by 4
+    //not divisible by 100
+    //divisble by 400
+
+    if(year % 4 == 0  && year % 100 != 0){
+        return true;
+    }
+    else if(year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 
 };
 
